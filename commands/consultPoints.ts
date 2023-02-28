84cb5105b7d40492c7f10303b241d11a18afc9b5
marcusmmmz <marcusizabel230@gmail.com>
2023-02-28 19:33:27 -0300

    consult only your own points

--- commands/consultPoints.ts
+++ commands/consultPoints.ts
@@ -1,29 +1,19 @@
-import { SlashCommand, CommandOptionType, SlashCreator, CommandContext } from 'slash-create';
+import { SlashCommand, SlashCreator, CommandContext } from 'slash-create';
 import { prisma } from '../db';
 
 export default class HelloCommand extends SlashCommand {
   constructor(creator: SlashCreator) {
     super(creator, {
       name: 'consultar',
-      description: "Consult a user's points",
-      options: [
-        {
-          type: CommandOptionType.USER,
-          name: 'user',
-          description: 'User to be consulted'
-        }
-      ]
+      description: "Consult a user's points"
     });
   }
 
   async run(ctx: CommandContext) {
     const user = await prisma.user.findUnique({
-      where: { id: ctx.options.user ?? ctx.user.id }
+      where: { id: ctx.user.id }
     });
 
-    const points = user?.points ?? 0;
-
-    if (ctx.options.user) return `<@${ctx.options.user}> has ${points} points`;
-    else return `You have ${points} points`;
+    return `You have ${user?.points ?? 0} points`;
   }
 }
