a14262059c6b0b90f460da7a9f42bb912060e487
marcusmmmz <marcusizabel230@gmail.com>
2023-02-28 21:59:04 -0300

    claim reward

---
+++ commands/claimReward.ts
@@ -0,0 +1,60 @@
+import { SlashCommand, SlashCreator, CommandContext, CommandOptionType } from 'slash-create';
+import { prisma } from '../db';
+
+export default class HelloCommand extends SlashCommand {
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'rewards',
+      description: 'Lists all rewards',
+      options: [
+        {
+          type: CommandOptionType.INTEGER,
+          name: 'id',
+          description: "The reward's id",
+          required: true
+        }
+      ]
+    });
+  }
+
+  async run(ctx: CommandContext) {
+    let [user, reward] = await Promise.all([
+      prisma.user.findUnique({
+        where: { id: ctx.user.id }
+      }),
+      prisma.reward.findUnique({
+        where: { id: ctx.options.id }
+      })
+    ]);
+
+    if (!reward) return "This reward doesn't exist";
+
+    const points = user?.points ?? 0;
+
+    if (points < reward.price) return "You don't have enough points to claim this reward";
+
+    await Promise.all([
+      prisma.user.upsert({
+        where: { id: ctx.user.id },
+        create: {
+          id: ctx.user.id,
+          points: 0
+        },
+        update: {
+          points: {
+            decrement: reward.price
+          }
+        }
+      }),
+      prisma.userReward.create({
+        data: {
+          rewardId: reward.id,
+          userId: ctx.user.id,
+          pending: true
+        }
+      })
+    ]);
+
+    return 'Reward successfully claimed';
+  }
+}
